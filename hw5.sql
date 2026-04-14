67860314d7698e0db1ed72662e0f98ecad37871b
AntoninaD123 <a.diachenko2023@gmail.com>
2026-04-14 12:49:22 -0400

    Add files via upload

---
+++ hw5.sql
@@ -0,0 +1,65 @@
+USE hw3_db;
+SHOW TABLES;
+
+SELECT
+    od.*,
+    (
+        SELECT o.customer_id
+        FROM orders o
+        WHERE o.id = od.order_id
+    ) AS customer_id
+FROM order_details od;
+
+SELECT *
+FROM order_details od
+WHERE od.order_id IN (
+   SELECT o.id
+   FROM orders o
+   WHERE o.shipper_id = 3
+);
+
+SELECT 
+   temp.order_id,
+   AVG(temp.quantity) AS avg_quantity
+FROM  (
+    SELECT 
+        order_id,
+        quantity
+	FROM order_details
+    WHERE quantity > 10
+) AS temp
+GROUP BY temp.order_id;
+
+WITH temp AS (
+    SELECT 
+        order_id,
+        quantity
+	FROM order_details
+    WHERE quantity > 10
+)
+SELECT 
+	order_id,
+    AVG(quantity) AS avg_quantity
+FROM temp
+GROUP BY order_id; 
+
+DROP FUNCTION IF EXISTS divide_values;
+
+DELIMITER $$
+
+CREATE FUNCTION divide_values(a FLOAT, b FLOAT)
+RETURNS FLOAT
+DETERMINISTIC
+BEGIN 
+   RETURN a / b;
+END $$
+
+DELIMITER ;    
+   
+SELECT
+    id,
+    order_id,
+    quantity,
+    divide_values(quantity, 2) AS divided_quantity
+FROM order_details;
+   
